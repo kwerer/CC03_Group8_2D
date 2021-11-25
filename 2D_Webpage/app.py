53671abfe4c9db91837735e5cfa5b773544bdde6
Joseph <josephlailailai247@gmail.com>
2021-11-26 05:51:58 +0800

    flask done

--- 2D_Webpage/app.py
+++ 2D_Webpage/app.py
@@ -1,22 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from forms import FeatureInput
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'lucky8'
 
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 def home():
-    return render_template("home.html")
+    beta_values = [52.73979899, -0.00207458, -6.91533959, 0.11199107, -0.8223395, 4.06842093, -0.83646151, 2.93171969]
+    column_ave_values = [60.11932, 8814.446, 0.02173, 1.0859, 6.24012, 19.184,0.01912]
+    column_std_values = [138.61, 4116.576, 0.04954, 0.3714, 4.08898, 29.919, 0.04558]
+    form = FeatureInput()
+    if form.is_submitted():
+        result = request.form
+        value_ls = []
+        for key, value in result.items():
+            if value != "Submit":
+                value_ls.append(float(value))
+            else:
+                value_ls.append(30)
+        val_norm = []
+        for index in range(0,7):
+            val_norm.append((value_ls[index] - column_ave_values[index]) / (column_std_values[index]))
+        sum = 0
+        sum = beta_values[0]
+        for c_index in range(1,7):
+            sum += beta_values[c_index] * val_norm[c_index-1]
+        val_norm.insert(0,"nil")
+        return render_template("results.html", result = result, beta_values = beta_values, val_norm=val_norm, value_ls = value_ls, sum = sum)
+    return render_template("home.html", form = form)
 
-@app.route("/task1")
-def task1():
-    return render_template("task1.html")
 
-@app.route("/task2")
-def task2():
-    return render_template("task2.html")
 
-@app.route("/task3")
-def task3():
-    return render_template("task3.html")   
 
 
 if __name__ == "__main__":
