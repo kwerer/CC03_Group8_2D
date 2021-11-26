1b522fb13adb841b8545ee3c6fabbfdc142fd052
Joseph <josephlailailai247@gmail.com>
2021-11-26 14:54:10 +0800

    ok

--- 2D_Webpage/app.py
+++ 2D_Webpage/app.py
@@ -9,6 +9,7 @@ def home():
     beta_values = [52.73979899, -0.00207458, -6.91533959, 0.11199107, -0.8223395, 4.06842093, -0.83646151, 2.93171969]
     column_ave_values = [60.11932, 8814.446, 0.02173, 1.0859, 6.24012, 19.184,0.01912]
     column_std_values = [138.61, 4116.576, 0.04954, 0.3714, 4.08898, 29.919, 0.04558]
+    features_names = ["Constant","New Cases Per Million", "Total Cases Per Million", "New Deaths Smoothed Per Million", " Repduction Rate", "New Tests Smoothed Per Thousand", "People Fully Vaccinated Per Hundered", "Positive Rate"]
     form = FeatureInput()
     if form.is_submitted():
         result = request.form
@@ -26,7 +27,8 @@ def home():
         for c_index in range(1,7):
             sum += beta_values[c_index] * val_norm[c_index-1]
         val_norm.insert(0,"nil")
-        return render_template("results.html", result = result, beta_values = beta_values, val_norm=val_norm, value_ls = value_ls, sum = sum)
+        value_ls.insert(0,"nil")
+        return render_template("results.html", result = result, beta_values = beta_values, val_norm=val_norm, value_ls = value_ls, sum = sum, features_names=features_names)
     return render_template("home.html", form = form)
 
 
